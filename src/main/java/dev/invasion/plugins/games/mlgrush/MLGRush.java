72d5060f1127315901f04eb91619a4ddd9ecc724
toqix <64910316+toqix@users.noreply.github.com>
2020-08-25 12:34:25 +0200

    Initial Commit

---
+++ src/main/java/dev/invasion/plugins/games/mlgrush/MLGRush.java
@@ -0,0 +1,17 @@
+package dev.invasion.plugins.games.mlgrush;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MLGRush extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
